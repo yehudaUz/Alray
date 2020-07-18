0be41c8de4d200adc012bd75dda4bee881791215
mudale222 <yuziel@walla.co.il>
2020-07-18 21:14:38 +0300

    responsive added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,15 @@ import HeaderField from './HeaderField'
 import Currency from './Currency'
 import HomeLink from './HomeLink'
 import { UseViewPort } from './UseViewPort'
+import { Collapse } from 'react-collapse';
+
 
 const Header = (props) => {
     const { width } = UseViewPort();
     const breakpoint = 1200;
+
+    const [isOpen, setOpen] = React.useState(false);
+
 
     return (
         <div className="header-wrapper">
@@ -25,8 +30,15 @@ const Header = (props) => {
                         <HeaderField text="דף הבית" link="/" />
                     </div>
                     :
-                    <div className="header-menu-button">
+                    <div className="header-menu-button" onClick={() => setOpen(!isOpen)}>
                         <i className="fa fa-bars" aria-hidden="true"></i>
+                        <Collapse isOpened={isOpen}>
+                            <HeaderField text="צור קשר" link="/contact" />
+                            <HeaderField text="טיולים מאורגנים" link="/orginaizedTrips" />
+                            <HeaderField text="טיסות" link="/flights" />
+                            <HeaderField text="חבילות נופש" link="/holidaysPackges" />
+                            <HeaderField text="דף הבית" link="/" />
+                        </Collapse>
                     </div>
                 }
             </div>
